afb9a6453bb568322a5f3eba1b60429b1aeaafdf
7une9ro <racoon121238@gmail.com>
2026-04-13 22:09:29 +0900

    UserService 생성

---
+++ src/main/java/com/example/www/domain/user/service/UserService.java
@@ -0,0 +1,19 @@
+package com.example.www.domain.user.service;
+
+import com.example.www.domain.user.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+
+}
