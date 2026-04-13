e504d4c6121c664e7e0aca0dd5c626338576dcf2
7une9ro <racoon121238@gmail.com>
2026-04-13 22:45:32 +0900

    UserResponseDTO 생성

---
+++ src/main/java/com/example/www/domain/user/dto/UserResponseDTO.java
@@ -0,0 +1,13 @@
+package com.example.www.domain.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponseDTO {
+
+    private String username;
+    private String nickname;
+    private String role;
+}
