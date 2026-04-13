a75a674b72799aaa8611c60034fd798e28f112de
7une9ro <racoon121238@gmail.com>
2026-04-13 22:25:32 +0900

    User 엔티티 Create 메서드 초기 구현

--- src/main/java/com/example/www/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/www/domain/user/repository/UserRepository.java
@@ -4,4 +4,7 @@ import com.example.www.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    // UserRepository에서 DB에 username이 있는지 확인하기 위한 메서드
+    Boolean existsByUsername(String username);
 }
--- src/main/java/com/example/www/domain/user/service/UserService.java
+++ src/main/java/com/example/www/domain/user/service/UserService.java
@@ -1,8 +1,12 @@
 package com.example.www.domain.user.service;
 
+import com.example.www.domain.user.dto.UserRequestDTO;
+import com.example.www.domain.user.entity.UserEntity;
+import com.example.www.domain.user.entity.UserRoleType;
 import com.example.www.domain.user.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -15,5 +19,26 @@ public class UserService {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+    // User 생성 (Create)
+    @Transactional
+    public void createOneUser(UserRequestDTO dto) {
+        String username = dto.getUsername();
+        String password = dto.getPassword();
+        String nickname = dto.getNickname();
 
+        // 생성하기 전, 동일한 username이 있는지 확인
+        if(userRepository.existsByUsername(username)) {
+            return;
+        }
+
+        // User 엔티티 생성
+        UserEntity entity = new UserEntity();
+        entity.setUsername(username);
+        entity.setUsername(bCryptPasswordEncoder.encode(password));
+        entity.setNickname(nickname);
+        entity.setRole(UserRoleType.USER);
+
+        // User 엔티티 저장소에 저장
+        userRepository.save(entity);
+    }
 }
